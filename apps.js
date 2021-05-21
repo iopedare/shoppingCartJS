598433d0fc6d85ec476f2abe9739f04a87b9d400
Dayo Opedare <djetlii@live.com>
2021-05-21 10:26:29 +0100

    displaying product

--- apps.js
+++ apps.js
@@ -33,7 +33,27 @@ class Products {
     }
 }
 
-class UI {}
+class UI {
+    displayProducts(products) {
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        Add to bag
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+            `
+        });
+        productsDOM.innerHTML = result;
+    }
+}
 
 class Storage {}
 
@@ -42,5 +62,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = new Products();
 
     //get all product
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => ui.displayProducts(products));
 });
