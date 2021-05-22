64392828d70a341483d1a04920482acaadd2506d
Dayo Opedare <djetlii@live.com>
2021-05-22 20:40:22 +0100

    showCart

--- apps.js
+++ apps.js
@@ -77,12 +77,51 @@ class UI {
                 // save cart in local storage
                 Storage.saveCart(cart);
                 // set cart values
+                this.setCartValues(cart);
                 // display cart item
+                this.addCartItem(cartItem);
                 // show the cart
+                this.showCart();
             });
                 
         
         });
+    }
+
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+
+    addCartItem(item) {
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `
+        <img src="${item.image} alt="product">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+        </div>
+        <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount" data-id=${item.amount}>1</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+        </div>            
+        `;
+        cartContent.appendChild(div);
+        console.log(cartContent);
+    }
+
+    showCart() {
+        cartOverlay.classList.add('transparentBcg');
+        cartDOM.classList.add('showCart');
     }
 }
 
